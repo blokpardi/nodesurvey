741052edccef785f6b6ca5f2caf5e7b8835742fa
Bill Pardi <bpardi@microsoft.com>
2019-12-03 16:27:27 -0800

    change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config.keys');
+const keys = require('./config/keys');
 const app = express();
 
 passport.use(
@@ -23,6 +23,8 @@ app.get(
     scope: ['profile', 'email']
   })
 );
+
+app.get('auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
