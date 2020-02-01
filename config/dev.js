c4938c75416889b82ff96a47700363bc5c070abf
Bill Pardi <bpardi@microsoft.com>
2020-02-01 13:39:31 -0800

    prod update

---
+++ config/dev.js
@@ -0,0 +1,11 @@
+//DEV.JS
+module.exports = {
+    googleClientID:
+      '1041622023379-1sepehpo3rk85obtmi210hs0pdehr3ri.apps.googleusercontent.com',
+    googleClientSecret: 'A57iI-r9xVJAevrA5G1fPt5L',
+    msidClientID: '43216ed8-c17d-4d5f-9507-cf518c5de4d0',
+    msidClientSecret: '@O7cIqcXA_aT4mNPUewQqI3ubw0sqO==',
+    mongoURI:
+      'mongodb+srv://bpmongo:MAZ8SAU3Jpondn5B@emaily-atv2e.azure.mongodb.net/test?retryWrites=true&w=majority',
+    cookieKey: 'sdkjasdfkjhkjhsdfadjhweskjhewrkjhasdkfjhwerkjhksjdfhskdjhfwe'
+  };
---
+++ config/prod.js
@@ -0,0 +1,10 @@
+// production keys
+//DEV.JS
+module.exports = {
+    googleClientID:process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    msidClientID: process.env.MSID_CLIENT_ID,
+    msidClientSecret: process.env.MSID_CLIENT_SECRET,
+    mongoURI:process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+  };
